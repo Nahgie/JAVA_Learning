5c6f17bef01e96676dfb3eb4fcd0d0488cc7f74b
Nahgie <133487853+Nahgie@users.noreply.github.com>
2023-05-30 23:25:46 +0900

    누락된 소스 체크

---
+++ JAVA_STUDY/src/LoopTest/Loop_for_2.java
@@ -0,0 +1,17 @@
+package LoopTest;
+
+public class Loop_for_2 {
+    public static void main (String[] args)throws Exception{
+
+        for(int i = 2 ; i <=9 ; i++){
+
+            for(int j = 1 ; j < 9 ; j++){
+
+                System.out.println(i + " x " + j + " = " + j*i);
+
+            }
+            System.out.println();
+        }
+
+    }
+}
